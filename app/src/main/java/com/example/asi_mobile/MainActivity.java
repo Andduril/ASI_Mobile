c53951d431d01a8044be26e06e6fae6119def70a
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2024-05-14 16:51:48 +0200

    Remove example

--- app/src/main/java/com/example/asi_mobile/MainActivity.java
+++ app/src/main/java/com/example/asi_mobile/MainActivity.java
@@ -1,21 +1,9 @@
 package com.example.asi_mobile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,18 +14,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         database = FirebaseDatabase.getInstance("https://asi-mobile-1bc67-default-rtdb.europe-west1.firebasedatabase.app/");
-    }
-
-    public void onAddUserClick(View view) {
-        DatabaseReference usersRef = database.getReference("users");
-
-        usersRef.setValue(new User("frodon", "frodon.sacquet@gmail.com"))
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.i("DB", "User added");
-                        Toast.makeText(MainActivity.this ,"Added", Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 }
