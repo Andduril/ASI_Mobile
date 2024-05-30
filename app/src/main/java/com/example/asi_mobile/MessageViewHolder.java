58802b8958dbe32e87048caa10ebd08fec1e541a
ElodieBlairet <82451222+ElodieBlairet@users.noreply.github.com>
2024-05-30 15:32:29 +0200

    Affichage droite-gauche en fonction de l'utilisateur connecté

--- app/src/main/java/com/example/asi_mobile/MessageViewHolder.java
+++ app/src/main/java/com/example/asi_mobile/MessageViewHolder.java
@@ -11,21 +11,35 @@ import com.example.asi_mobile.Models.Message;
 import java.util.Objects;
 
 public class MessageViewHolder extends RecyclerView.ViewHolder {
-    //Autant d'attributs dans la classe que de widgets affichés (content et userId : 2)
+    //Autant d'attributs dans la classe que de widgets affichés (content, userId et timestamp : 3)
     private TextView contentTextView;
     private TextView userIdTextView;
+    private TextView timestampTextView;
+    private TextView contentConnectedUserTextView;
 
     public MessageViewHolder(@NonNull View itemView) {
         super(itemView);
 
-        this.contentTextView = itemView.findViewById(R.id.textView_content_recycler);
-        this.userIdTextView = itemView.findViewById(R.id.textView_userId_recycler);
+        this.contentTextView = itemView.findViewById(R.id.textView_content_autreUser);
+        this.userIdTextView = itemView.findViewById(R.id.textView_userId);
+        this.timestampTextView = itemView.findViewById(R.id.textView_timestamp);
+        this.contentConnectedUserTextView = itemView.findViewById(R.id.textView_content_connectedUser);
     }
 
     public void MettreAJourLigne(Message unMessage) {
         if (Objects.nonNull(unMessage)) {
-            this.contentTextView.setText(unMessage.getContent());
+            if (Objects.equals(unMessage.getUserId(), "fakeID"))//Si le userId est celui de l'utilisateur connecté
+            {
+                this.contentConnectedUserTextView.setText(unMessage.getContent());
+                this.contentTextView.setText("");
+            }
+            else
+            {
+                this.contentTextView.setText(unMessage.getContent());
+                this.contentConnectedUserTextView.setText("");
+            }
             this.userIdTextView.setText(unMessage.getUserId());
+            this.timestampTextView.setText(unMessage.getTimestamp());
         }
     }
 }
